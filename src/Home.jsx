5e74efcce740d694e80af62a82d579cdb4b95929
Nirav Patel <127055509+nrpatel370@users.noreply.github.com>
2024-09-28 15:10:53 -0400

    updated Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div id="header">
+    <div className="container">
+        <nav>
+            {/* <img src="images/logo.png" class="logo"> */}
+            <ul id="sidemenu">
+                <li><a href="#header">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#map">Map</a></li>
+                <li><a href="#contact">Contact</a></li>
+
+                <i className="fa-solid fa-xmark" onClick="closemenu()"></i>
+            </ul>
+            <i className="fa-solid fa-bars" onClick="openmenu()"></i>
+        </nav>
+        <div className="header-text">
+            <p>Junior CS Student at Wayne State University</p>
+            <h1>Hi! I'm <span>Mohammed</span> <br>Muneeb</br> </h1>
+        </div>
+    </div>
+
+
     </div>
   )
 }
