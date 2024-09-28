ab156065ad8a36942413719984762ff752005302
Kaavya Karnati <kaavya.karnati@yottaasys.com>
2024-09-28 18:53:57 -0400

    User Input

--- backend/CityNews.py
+++ backend/CityNews.py
@@ -1,9 +1,13 @@
 import requests
 
+userInput = input("Enter the City Name: ")
+
 url = 'https://magicloops.dev/api/loop/run/2393f643-8919-4662-b196-386f92c3ddca'
-payload = { 'input': 'Ann Arbor' }
+payload = { 'input': userInput }             # Changing to a user input
 
 response = requests.get(url, json=payload)
 responseJson = response.json()
 print(f"STATUS: {responseJson['status']}")
 print(f"OUTPUT: {responseJson['loopOutput']}")
+
+
