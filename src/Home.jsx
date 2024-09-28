119c3ea5771b4481c9bed2a19b1192c859188bce
Nirav Patel <127055509+nrpatel370@users.noreply.github.com>
2024-09-28 15:33:25 -0400

    Home component set up

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,14 +11,13 @@ const Home = () => {
                 <li><a href="#about">About</a></li>
                 <li><a href="#map">Map</a></li>
                 <li><a href="#contact">Contact</a></li>
-
-                <i className="fa-solid fa-xmark" onClick="closemenu()"></i>
+                
             </ul>
-            <i className="fa-solid fa-bars" onClick="openmenu()"></i>
+           
         </nav>
         <div className="header-text">
-            <p>Junior CS Student at Wayne State University</p>
-            <h1>Hi! I'm <span>Mohammed</span> <br>Muneeb</br> </h1>
+            <p>Welcome to Mood Map!</p>
+            <h1>Mood Map Description</h1>
         </div>
     </div>
 
