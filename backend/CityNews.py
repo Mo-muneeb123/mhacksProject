27244d1b5bc19e439438ed73df2ce4986a641a32
Kaavya Karnati <kaavya.karnati@yottaasys.com>
2024-09-28 20:07:29 -0400

    Got the Coordinates

--- backend/CityNews.py
+++ backend/CityNews.py
@@ -10,4 +10,35 @@ responseJson = response.json()
 print(f"STATUS: {responseJson['status']}")
 print(f"OUTPUT: {responseJson['loopOutput']}")
 
+# Second API
+
+url = 'https://magicloops.dev/api/loop/run/6d8f55bf-00d4-429a-8e81-3452b99d22c5'
+payload = { 'input': userInput }
+
+response = requests.get(url, json=payload)
+responseJson = response.json()
+print(f"STATUS: {responseJson['status']}")
+print(f"OUTPUT: {responseJson['loopOutput']}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
