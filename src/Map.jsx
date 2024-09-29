43100f380ec7c7147ce4103013b05772f8d093d0
Nirav Patel <127055509+nrpatel370@users.noreply.github.com>
2024-09-29 09:15:00 -0400

    added search box

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,9 +1,64 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { useState, useEffect } from 'react';
 
 
 const Map = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [savedSearch, setSavedSearch] = useState('');
+
+    const styles = {
+      container: {
+          textAlign: 'center',
+          padding: '20px',
+      },
+      input: {
+          padding: '10px',
+          width: '250px',
+          border: '2px solid #ccc',
+          borderRadius: '5px',
+          marginRight: '10px',
+          display: 'flex', 
+          justifyContent: "center",
+          alignItems: 'center'
+
+      },
+      button: {
+          padding: '10px 15px',
+          borderRadius: '5px',
+          backgroundColor: '#4CAF50',
+          color: 'white',
+          border: 'none',
+          cursor: 'pointer',
+          display: 'flex',
+          justifyContent: "center",
+          alignItems: 'center'
+
+      },
+      savedSearch: {
+          marginTop: '20px',
+          color: '#333',
+      }
+  };
+
+    const handleInputChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSavedSearch(searchTerm); 
+        setSearchTerm(''); 
+    };
+
+    useEffect(() => {
+        if (searchTerm !== '') {
+            console.log(`Current search term: ${searchTerm}`);
+           
+        }
+    }, [searchTerm]);
+
     return (
       <>
       <div id ='map'>
@@ -12,6 +67,26 @@ const Map = () => {
         <div className="map-cointainer"></div>
       </div>
       </div>
+
+      <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Enter a Valid State/City Name in the USA"
+                    value={searchTerm}
+                    onChange={handleInputChange}
+                />
+                <button type="submit">Search</button>
+            </form>
+            {savedSearch && (
+                <div>
+                    <h3>Saved Search:</h3>
+                    <p>{savedSearch}</p>
+                </div>
+            )}
+        </div>
+
+      
 
         <MapContainer  center={[37.8, -96]} zoom={4} scrollWheelZoom={true} style={{ height: '80vh', width: '90%',
             display:'flex', justifyContent:'center', alignItems:'center', margin: '0 auto' }} >
