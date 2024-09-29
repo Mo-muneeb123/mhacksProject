202f482a398d5cd22420bda6e0b68d9e338101fb
Parthiv12 <sign2parthiv@gmail.com>
2024-09-29 08:52:03 -0400

    add map

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 const Map = () => {
-  return (
-    <div id="map">
-    <div className="container">
-        <h1 className="sub-title">Live Sentiment Map</h1>
-        <div className="map-container">
-        </div>
-    </div>
-</div>
+    return (
+      <>
+      <div id ='map'>
+      <div className ='container'>
+        <h1 className ='sub-title'>Live Sentiment Map</h1>
+        <div className="map-cointainer"></div>
+      </div>
+      </div>
 
-  )
-}
+        <MapContainer center={[37.8, -96]} zoom={4} scrollWheelZoom={true} style={{ height: '100vh', width: '100%', paddingLeft: '30px' }}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={[37.8, -96]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
+      </>
+      
+    );
+};
 
-export default Map
+export default Map;
